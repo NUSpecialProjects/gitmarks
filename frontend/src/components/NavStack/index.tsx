292126e71ffc915f14b1d10fe1200865e554c1e2
Cam Plume <116120547+CamPlume1@users.noreply.github.com>
2024-10-03 11:49:38 -0400

    Proof of concept for deployment

--- frontend/src/components/NavStack/index.tsx
+++ frontend/src/components/NavStack/index.tsx
@@ -7,7 +7,7 @@ const NavStack: React.FC = () => {
     { name: "Grading", dest: "/grading", Icon: FaStream },
     { name: "Assignments", dest: "/assignments", Icon: FaUsers },
     { name: "Settings", dest: "/settings", Icon: FaCog },
-    {name: "Stubs", dest: "/stubs", Icon: FaCog}
+    {name: "Stub", dest: "/stub", Icon: FaCog}
   ];
 
   return (
