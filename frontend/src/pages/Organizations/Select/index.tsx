04e5c0d0be2a88b37e123d50891b285b4a76f415
Nick <tietje.n@northeastern.edu>
2025-01-25 10:03:22 -0500

    remove auto select

--- frontend/src/pages/Organizations/Select/index.tsx
+++ frontend/src/pages/Organizations/Select/index.tsx
@@ -1,28 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import OrganizationDropdown from "@/components/Dropdown/Organization";
 import Panel from "@/components/Panel";
 import Button from "@/components/Button";
-import { getOrganizationDetails } from "@/api/organizations";
-import { useCurrentClassroom } from "@/hooks/useClassroomUser";
 import { useAppInstallations } from "@/hooks/useOrganization";
 
 const OrganizationSelection: React.FC = () => {
   const [selectedOrg, setSelectedOrg] = useState<IOrganization | null>(null);
-  const { selectedClassroom } = useCurrentClassroom();
   const githubAppName = import.meta.env.VITE_GITHUB_APP_NAME;
   const { data: installationsData, isLoading: loadingOrganizations, error: installationsError } = useAppInstallations();
-
-  useEffect(() => { // set initially selected org
-    if (selectedClassroom) {
-      getOrganizationDetails(selectedClassroom.org_name)
-        .then(orgDetails => {
-          setSelectedOrg(orgDetails);
-        })
-        .catch();
-    }
-  }, [selectedClassroom]);
-
   const orgsWithApp = installationsData?.orgs_with_app || [];
   const orgsWithoutApp = installationsData?.orgs_without_app || [];
 
