74ad924b891c0f33e475a339a6fcfd428788466e
Nicholas Tietje <tietje.n@northeastern.edu>
2025-02-11 17:25:38 -0500

    fix merge changes

--- backend/internal/handlers/classrooms/classrooms.go
+++ backend/internal/handlers/classrooms/classrooms.go
@@ -436,57 +436,6 @@ func (s *ClassroomService) useClassroomToken() fiber.Handler {
 	}
 }
 
-// Invites a user to a classroom and attempts to accept their invitation
-// This should be called on the target user's behalf
-func (s *ClassroomService) inviteUserToClassroom(ctx context.Context, classroomID int64, classroomRole models.ClassroomRole, invitee *models.User, userClient github.GitHubUserClient) (models.Classroom, models.ClassroomUser, error) {
-	// Get the classroom from the DB
-	classroom, err := s.store.GetClassroomByID(ctx, classroomID)
-	if err != nil {
-		return models.Classroom{}, models.ClassroomUser{}, errs.InternalServerError()
-	}
-
-	classroomUser, err := s.store.GetUserInClassroom(ctx, classroomID, *invitee.ID)
-	if err != nil {
-		classroomUser, err = s.store.AddUserToClassroom(ctx, classroomID, string(classroomRole), models.UserStatusRequested, *invitee.ID)
-		if err != nil {
-			return models.Classroom{}, models.ClassroomUser{}, errs.InternalServerError()
-		}
-	}
-
-	classroomUser, err = s.updateUserStatus(ctx, s.appClient, *invitee, classroom)
-	if err != nil {
-		return models.Classroom{}, models.ClassroomUser{}, errs.InternalServerError()
-	}
-
-	// don't do anything if the user has been removed from the classroom
-	if classroomUser.Status == models.UserStatusRemoved {
-		return models.Classroom{}, models.ClassroomUser{}, errs.StudentRemovedFromClassroomError()
-	}
-
-	// user is already in the classroom. If their role can be upgraded, do so. Don't downgrade them.
-	roleComparison := classroomUser.Role.Compare(classroomRole)
-	if roleComparison < 0 {
-		// Upgrade the user's role in the classroom
-		classroomUser, err = s.store.ModifyUserRole(ctx, classroomID, string(classroomRole), *classroomUser.ID)
-		if err != nil {
-			return models.Classroom{}, models.ClassroomUser{}, errs.InternalServerError()
-		}
-	}
-
-	// Invite the user to the organization
-	classroomUser, err = s.inviteUserToOrganization(ctx, s.appClient, classroom, classroomRole, *invitee)
-	if err != nil {
-		return models.Classroom{}, models.ClassroomUser{}, errs.InternalServerError()
-	}
-
-	// Accept the pending invitation to the organization
-	err = s.acceptOrgInvitation(ctx, userClient, classroom.OrgName, classroomID, *invitee)
-	if err != nil {
-		return models.Classroom{}, models.ClassroomUser{}, errs.InternalServerError()
-	}
-	return classroom, classroomUser, nil
-}
-
 // Returns the user's status in the classroom, nil if not in the classroom
 func (s *ClassroomService) getCurrentClassroomUser() fiber.Handler {
 	return func(c *fiber.Ctx) error {
@@ -683,19 +632,10 @@ func (s *ClassroomService) getClassroomNames() fiber.Handler {
 				continue // Skip if month not found in map
 			}
 			semesterName := fmt.Sprintf("%s %d", semester, year)
-			if !stringInList(semesterNames, semesterName) {
+			if !utils.Contains(semesterNames, semesterName) {
 				semesterNames = append(semesterNames, semesterName)
 			}
 		}
 		return c.Status(http.StatusOK).JSON(fiber.Map{"semester_names": semesterNames})
 	}
-}
-
-func stringInList(list []string, str string) bool {
-	for _, item := range list {
-		if item == str {
-			return true
-		}
-	}
-	return false
 }
