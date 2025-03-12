11141e571911e793bc9789d8d63eebcaf518595a
Nick <tietje.n@northeastern.edu>
2025-03-11 20:51:42 -0400

    add comments

--- frontend/src/components/Toast/index.tsx
+++ frontend/src/components/Toast/index.tsx
@@ -1,7 +1,7 @@
 import { toast } from "react-toastify";
 import "./styles.css";
 
-
+// Displays an error toast
 export function ErrorToast(text: string, toastId: string) {
   return toast.error(text, {
     position: "top-right",
@@ -16,6 +16,7 @@ export function ErrorToast(text: string, toastId: string) {
   });  
 }
 
+// Displays a success toast
 export function SuccessToast(text: string, toastId: string) {
   return toast.success(text, {
     position: "top-right",
@@ -25,6 +26,7 @@ export function SuccessToast(text: string, toastId: string) {
   });
 }
 
+// Displays an info toast
 export function InfoToast(text: string, toastId: string) {
   return toast.info(text, {
     position: "top-right",
@@ -36,6 +38,13 @@ export function InfoToast(text: string, toastId: string) {
 
 type ToastCallback = () => Promise<void>;
 
+/**
+ * This hook is used to display a toast when an action is performed.
+ * It takes in a toastId, an actionCallback, and a messages object which determines what to display based on the current status of the action.
+ * The actionCallback is the function to execute and is expected to return a promise.
+ * Typically, executeWithToast will be called within a try, finally block but without a catch block as the toast will handle errors.
+ * 
+ */
 export const useActionToast = () => {
   const executeWithToast = async (
     toastId: string,
