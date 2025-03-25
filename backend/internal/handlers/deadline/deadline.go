fa522d922905d13d3d5a4e376a2f3d366f5fd4bf
Cam Plume <116120547+CamPlume1@users.noreply.github.com>
2025-03-25 17:53:51 -0400

    Deadline refactor (#207)
    * added deadline SQL Queries and update statements
    
    * v1 deadline handling refactor
    
    * Working deadline handling refactor
    
    * oopsies

---
+++ backend/internal/handlers/deadline/deadline.go
@@ -0,0 +1,26 @@
+package deadline
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (s *DeadlineService) DeadlineHandler(c *fiber.Ctx) error {
+	repo := c.Params("repo")
+	if repo == "" {
+		// Intentionally unhelpful as this is a public endpoint
+		return c.Status(fiber.StatusBadRequest).JSON("Bad Request")
+	}
+	// Retrieve Deadline from DB
+	due, err := s.store.GetDeadlineForRepo(c.Context(), repo)
+	if err != nil {
+		fmt.Printf(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON("Error Retrieving Assignment Deadline")
+	}
+
+	// return true if the repo is overdue, false if not
+	return c.Status(fiber.StatusOK).JSON(map[string]bool{"overdue": due.Before(time.Now())})
+
+}
